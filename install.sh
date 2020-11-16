1015940ffd4908be0244f962d33ea263d8d4089c
nan-dre <andu_niculae@yahoo.com>
2020-11-16 21:07:13 +0200

    Removed some config files

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ DATE=$(date +"%Y_%m_%d-%H:%M:%S")
 [ -f ~/.bashrc ] && echo "Found .bashrc, moving to backups." && mv ~/.bashrc ./backups/.bashrc_$DATE.bak
 ln -sr ./.bashrc ~/
 
-[ -f ~/.tmux.conf ] && echo "Found .tmux.conf, moving to backups." && mv ~/.tmux.conf ./backups/.tmux.conf_$DATE.bak
+#[ -f ~/.tmux.conf ] && echo "Found .tmux.conf, moving to backups." && mv ~/.tmux.conf ./backups/.tmux.conf_$DATE.bak
 ln -sr ./.tmux.conf ~/
 
 [ -f ~/.vimrc ] && echo "Found .vimrc, moving to backups." && mv ~/.vimrc ./backups/.vimrc_$DATE.bak
