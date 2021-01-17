589472821b71658d6fd7e77ef6bd46791cde69e0
Nandre <47075034+nan-dre@users.noreply.github.com>
2021-01-17 22:58:45 +0200

    Delete install_nvim.sh

--- install_nvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/Optixal/neovim-init.vim.git
-novim-init.vim/install.sh
-rm -r neovim-init.vim
-
-[ -f ~/.config/nvim/init.vim ] && echo "Found init.vim, moving to backups." && mv ~/.config/nvim/init.vim ./backups/init.vim_$DATE.bak
-ln -sr .config/nvim/init.vim ~/.config/nvim/
