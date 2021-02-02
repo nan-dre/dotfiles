87fedf9a07f4c523d7354396ec3ac8b139e814a5
nan-dre <andu_niculae@yahoo.com>
2021-02-02 23:31:28 +0200

    backups

--- apps/tmux/install.sh
+++ apps/tmux/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt install tmux -y
+[ ! -d ./backups ] && mkdir ./backups
+[ -f ~/.tmux.conf ] && echo "Found .tmux.conf, moving to backups." && mv ~/.tmux.conf ./backups/.tmux.conf_$DATE.bak
 ln -s ~/.dotfiles/apps/tmux/.tmux.conf ~/
