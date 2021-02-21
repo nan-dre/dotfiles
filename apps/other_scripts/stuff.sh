338945e40cea956384c6a9f1973ffce7bbe430bf
nan-dre <andu_niculae@yahoo.com>
2021-02-21 17:05:51 +0200

    symbols

--- apps/other_scripts/stuff.sh
+++ apps/other_scripts/stuff.sh
@@ -2,3 +2,5 @@ sudo apt update && sudo apt install git tldr locate
 
 git config --global user.email "andu_niculae@yahoo.com"
 git config --global user.name "nan-dre"
+
+cp ./us /usr/share/X11/xkb/symbols/us
