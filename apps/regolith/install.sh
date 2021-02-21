7f62adfd2b267b048c433b1397765ae26c9129d3
nan-dre <andu_niculae@yahoo.com>
2021-02-21 15:58:52 +0200

    new install script

--- apps/regolith/install.sh
+++ apps/regolith/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ln -nsfr ~/.dotfiles/apps/regolith ~/.config/
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt-get install regolith-desktop-standard -y
+#ln -nsfr ~/.dotfiles/apps/regolith ~/.config/
+ln -sr i3/ ~/.config/regolith/
+ln -sr i3status/ ~/.config/regolith/
+ln -sr i3xrock/ ~/.config/regolith/
