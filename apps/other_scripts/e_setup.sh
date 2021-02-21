fc0cf4426ed20c9a557bc8a8f5954161a0c2e4ad
nan-dre <andu_niculae@yahoo.com>
2021-02-21 17:04:13 +0200

    regolith

--- apps/other_scripts/e_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-../tmux/install.sh 1.tmux.conf
-tmux source-file ~/.tmux.conf
-sudo apt install tldr locate -y
---
+++ apps/other_scripts/stuff.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt install git tldr locate
+
+git config --global user.email "andu_niculae@yahoo.com"
+git config --global user.name "nan-dre"
