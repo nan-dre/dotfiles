f94d65f88f82abc5512965b3c4c07ecd9b5c6cbc
nan-dre <andu_niculae@yahoo.com>
2021-02-27 10:07:24 +0200

    Added i3 config for laptop

--- apps/other_scripts/at_boot.sh
+++ apps/other_scripts/at_boot.sh
@@ -2,4 +2,3 @@
 
 setxkbmap -option caps:super
 killall xcape 2>/dev/null ; xcape -e 'Super_L=Escape'
-echo 50 | sudo tee -a /sys/class/backlight/amdgpu_bl0/brightness
--- apps/other_scripts/stuff.sh
+++ apps/other_scripts/stuff.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install git tldr locate
+#sudo apt update && sudo apt install git tldr locate
 
 git config --global user.email "andu_niculae@yahoo.com"
 git config --global user.name "nan-dre"
